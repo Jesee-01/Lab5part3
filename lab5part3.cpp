d121483b2c8874116afc604dc1e917d404150d45
Jesee-01 <78709010+Jesee-01@users.noreply.github.com>
2021-03-18 21:53:42 -0700

    Add files via upload

---
+++ lab5part3.cpp
@@ -0,0 +1,27 @@
+// lab5part3.cpp
+// Be sure to include comments where necessary for style grading
+//You're on your own for this one, be sure to include all necessary pieces
+//and be sure it looks exactly like the diagram with all matching symbols
+
+#include <iostream>            
+using namespace std;
+
+int main ()
+{
+    cout << "_______\n";
+    cout << "___^___\n";
+    cout << "__/*\\__\n";
+    cout << "_/***\\_\n";
+    cout << "|*****|\n";
+    cout << "_\\***/_\n";
+    cout << "__\\*/__\n";
+    cout << "___v___\n";
+    cout << "_______\n";
+    
+    //Instead of usind endl to end display and start a new one I used the alternative
+    // \n to replace endl
+    
+    
+    
+return 0;
+}
